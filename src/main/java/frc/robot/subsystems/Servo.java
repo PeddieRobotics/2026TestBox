93ecc87e04a3858126c9d9a6b01852a1accb4491
jamieq <jamieqiu15@gmail.com>
2026-02-16 16:05:53 -0500

    Update Servo.java

--- src/main/java/frc/robot/subsystems/Servo.java
+++ src/main/java/frc/robot/subsystems/Servo.java
@@ -17,49 +17,28 @@ public class Servo extends SubsystemBase{
     public static Servo servo;
 
     public Servo() {
-        servoHub = new ServoHub(3);
+        servoHub = new ServoHub(???);
 
-        channel0 = servoHub.getServoChannel(ChannelId.kChannelId0);
-        channel1 = servoHub.getServoChannel(ChannelId.kChannelId1);
-        channel2 = servoHub.getServoChannel(ChannelId.kChannelId2);
-        channel3 = servoHub.getServoChannel(ChannelId.kChannelId3);
-        channel4 = servoHub.getServoChannel(ChannelId.kChannelId4);
-        channel5 = servoHub.getServoChannel(ChannelId.kChannelId5);
+        // fill in this and do it for the rest of the channels!
+        channel0 = servoHub.getServoChannel(???);
 
+
+        // fill in this and do it for the rest of the channels!
         channel0.setPowered(true);
         channel0.setEnabled(true);
-        
-        channel1.setPowered(true);
-        channel1.setEnabled(true);
-
-        channel2.setPowered(true);
-        channel2.setEnabled(true);
-
-        channel3.setPowered(true);
-        channel3.setEnabled(true);
-
-        channel4.setPowered(true);
-        channel4.setEnabled(true);
-        
-        channel5.setPowered(true);
-        channel5.setEnabled(true);
 
         config = new ServoHubConfig();
-        config.channel0.pulseRange(500,1500,2500);
-        config.channel1.pulseRange(500,1500,2500);
-        config.channel2.pulseRange(500,1500,2500);
-        config.channel3.pulseRange(500, 1500, 2500);
-        config.channel4.pulseRange(500,1500,2500);
-        config.channel5.pulseRange(500,1500,2500);
+        config.channel0.pulseRange(???,???,???);
 
 
-        System.out.println("Configure status: " + servoHub.configure(config, ResetMode.kResetSafeParameters).toString());
+        servoHub.configure(config, ResetMode.kResetSafeParameters).toString();
 
-        System.out.println("3-5 status: " + servoHub.setBankPulsePeriod(ServoHub.Bank.kBank3_5, 5000).toString());
-        System.out.println("0-2 status: " + servoHub.setBankPulsePeriod(ServoHub.Bank.kBank0_2, 5000).toString());
+        servoHub.setBankPulsePeriod(???, ???).toString();
+        servoHub.setBankPulsePeriod(???, ???).toString();
 
-        SmartDashboard.putNumber("b2 pulse width", 1500);
-        SmartDashboard.putNumber("b3 pulse width", 1500);
+        // use this after servos are set up
+        // SmartDashboard.putNumber("b2 pulse width", 1500);
+        // SmartDashboard.putNumber("b3 pulse width", 1500);
 
     }
 
@@ -71,7 +50,7 @@ public class Servo extends SubsystemBase{
 
     @Override
     public void periodic() {
-        channel2.setPulseWidth((int)SmartDashboard.getNumber("b2 pulse width", 1500));
-        channel3.setPulseWidth((int)SmartDashboard.getNumber("b3 pulse width", 1500));
+        // channel2.setPulseWidth((int)SmartDashboard.getNumber("b2 pulse width", ???));
+        // channel3.setPulseWidth((int)SmartDashboard.getNumber("b3 pulse width", ???));
     }
 }
