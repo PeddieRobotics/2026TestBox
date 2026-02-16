8a2b1082321181aaccd88152574e0fe9e6819ead
meghana-i15 <mindupuru-29@peddie.org>
2026-02-16 17:34:59 -0500

    servo test

--- src/main/java/frc/robot/subsystems/Servo.java
+++ src/main/java/frc/robot/subsystems/Servo.java
@@ -12,54 +12,55 @@ import com.revrobotics.servohub.config.ServoHubConfig;
 public class Servo extends SubsystemBase{
 
     private ServoHubConfig config;
-    private ServoHub servoHub;
+    private ServoHub servohub;
     private ServoChannel channel0, channel1, channel2, channel3, channel4, channel5;
     public static Servo servo;
 
     public Servo() {
-        servoHub = new ServoHub(3);
-
-        channel0 = servoHub.getServoChannel(ChannelId.kChannelId0);
-        channel1 = servoHub.getServoChannel(ChannelId.kChannelId1);
-        channel2 = servoHub.getServoChannel(ChannelId.kChannelId2);
-        channel3 = servoHub.getServoChannel(ChannelId.kChannelId3);
-        channel4 = servoHub.getServoChannel(ChannelId.kChannelId4);
-        channel5 = servoHub.getServoChannel(ChannelId.kChannelId5);
+        servohub = new ServoHub(3);
+        
+        channel0 = servohub.getServoChannel(ChannelId.kChannelId0);
+        channel1 = servohub.getServoChannel(ChannelId.kChannelId1);
+        channel2 = servohub.getServoChannel(ChannelId.kChannelId2);
+        channel3 = servohub.getServoChannel(ChannelId.kChannelId3);
+        channel4 = servohub.getServoChannel(ChannelId.kChannelId4);
+        channel5 = servohub.getServoChannel(ChannelId.kChannelId5);
 
         channel0.setPowered(true);
         channel0.setEnabled(true);
-        
+
         channel1.setPowered(true);
         channel1.setEnabled(true);
 
         channel2.setPowered(true);
         channel2.setEnabled(true);
-
+        
         channel3.setPowered(true);
         channel3.setEnabled(true);
-
+        
         channel4.setPowered(true);
         channel4.setEnabled(true);
-        
+
         channel5.setPowered(true);
         channel5.setEnabled(true);
 
         config = new ServoHubConfig();
-        config.channel0.pulseRange(500,1500,2500);
-        config.channel1.pulseRange(500,1500,2500);
-        config.channel2.pulseRange(500,1500,2500);
+        config.channel0.pulseRange(500, 1500, 2500);
+        config.channel1.pulseRange(500, 1500, 2500);
+        config.channel2.pulseRange(500, 1500, 2500);
         config.channel3.pulseRange(500, 1500, 2500);
-        config.channel4.pulseRange(500,1500,2500);
-        config.channel5.pulseRange(500,1500,2500);
+        config.channel4.pulseRange(500, 1500, 2500);
+        config.channel5.pulseRange(500, 1500, 2500);
 
+        servohub.setBankPulsePeriod(ServoHub.Bank.kBank0_2,5000);
+        servohub.setBankPulsePeriod(ServoHub.Bank.kBank3_5,5000);
 
-        System.out.println("Configure status: " + servoHub.configure(config, ResetMode.kResetSafeParameters).toString());
-
-        System.out.println("3-5 status: " + servoHub.setBankPulsePeriod(ServoHub.Bank.kBank3_5, 5000).toString());
-        System.out.println("0-2 status: " + servoHub.setBankPulsePeriod(ServoHub.Bank.kBank0_2, 5000).toString());
-
-        SmartDashboard.putNumber("b2 pulse width", 1500);
-        SmartDashboard.putNumber("b3 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 0 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 1 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 2 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 3 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 4 pulse width", 1500);
+        SmartDashboard.putNumber("Channel 5 pulse width", 1500);
 
     }
 
@@ -71,8 +72,12 @@ public class Servo extends SubsystemBase{
 
     @Override
     public void periodic() {
-        channel2.setPulseWidth((int)SmartDashboard.getNumber("b2 pulse width", 1500));
-        channel3.setPulseWidth((int)SmartDashboard.getNumber("b3 pulse width", 1500));
+       channel0.setPulseWidth((int)SmartDashboard.getNumber("Channel 0 pulse width", 1500));
+       channel1.setPulseWidth((int)SmartDashboard.getNumber("Channel 1 pulse width", 1500));
+       channel2.setPulseWidth((int)SmartDashboard.getNumber("Channel 2 pulse width", 1500));
+       channel3.setPulseWidth((int)SmartDashboard.getNumber("Channel 3 pulse width", 1500));
+       channel4.setPulseWidth((int)SmartDashboard.getNumber("Channel 4 pulse width", 1500));
+       channel5.setPulseWidth((int)SmartDashboard.getNumber("Channel 5 pulse width", 1500));
     }
 }
 
